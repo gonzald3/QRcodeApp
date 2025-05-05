31ca613ed5e7fe553c8e766732ff10f478777f6c
Daniel Gonzalez <gonzald3@protonmail.com>
2025-05-05 15:06:56 -0400

    scan function

--- models/Scan.js
+++ models/Scan.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 
 const scanSchema = new mongoose.Schema({
-  code: String,
-  adId: String,
-  locationId: String,
-  locationName: String,
-  userSessionId: String,
-  ipAddress: String,
-  userAgent: String,
-  timestamp: { type: Date, default: Date.now }
+    code: String,
+    adId: String,
+    locationId: String,
+    locationName: String,
+    userSessionId: String,
+    ipAddress: String,
+    userAgent: String,
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
 });
 
-const Scan = mongoose.model('Scan', scanSchema);
-
-module.exports = Scan;
+module.exports = mongoose.model('Scan', scanSchema);
